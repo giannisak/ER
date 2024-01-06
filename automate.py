b825d1d2e66ceaed391aaf0b4dec73a7c770b9f2
giannisak <154079765+giannisak@users.noreply.github.com>
2024-01-06 13:23:02 +0200

    Update automate.py

--- automate.py
+++ automate.py
@@ -1,9 +1,11 @@
 import pandas as pd
 import numpy as np
+from sklearn.metrics import confusion_matrix, accuracy_score, precision_score, recall_score, f1_score
+import time
 
 # use custom ollama model "worker" on llama_index
 from llama_index.llms import Ollama
-llm = Ollama(model='worker')
+llm = Ollama(model='worker', request_timeout=180)
 
 # paths for datasets, candidate pairs and groundtruth files
 dataset_1 = 'dt_cp_gt/dt1_rest1.csv'
@@ -38,7 +40,11 @@ dt2 = dt2[:, 1:]
 dt1 = np.array([' '.join([x for x in row if isinstance(x, str)]) for row in dt1])
 dt2 = np.array([' '.join([x for x in row if isinstance(x, str)]) for row in dt2])
 
-for i in range(40):
+#main loop: model iterates through each pair and returns its responses
+start = time.time()
+responses = []
+
+for i in range(len(cp)):
     dt1_index = cp[i][0]
     dt2_index = cp[i][1]
 
@@ -52,6 +58,49 @@ for i in range(40):
     query = f"record 1: {r1}, record 2: {r2}. Answer with True. or False."
 
     resp = llm.complete(query)
+    responses.append(resp.text)
 
     print(f"worker's response: {resp}")
     print(" ")
+
+end = time.time()
+
+#model's response time
+time_seconds = end - start  
+hours, remainder = divmod(time_seconds, 3600)
+minutes, seconds = divmod(remainder, 60)
+print("Response Time: {:02}:{:02}:{:.2f}".format(int(hours), int(minutes), seconds))
+
+#model's responses
+print("Model's Responses:",responses)
+
+#model's 'good behavior' response rate
+good_responses = sum(response == 'True' or response == 'False' for response in responses)
+good_behavior_rate = good_responses / len(responses)
+print("Good Behavior Response Rate:", good_behavior_rate)
+
+#evaluation metrics
+true_labels = [1 if pair in gt else 0 for pair in cp]
+predicted_labels = [1 if resp == 'True' else 0 for resp in responses]
+
+conf_matrix = confusion_matrix(true_labels, predicted_labels)
+
+TP = conf_matrix[1, 1]
+FP = conf_matrix[0, 1]
+TN = conf_matrix[0, 0]
+FN = conf_matrix[1, 0]
+
+accuracy = accuracy_score(true_labels, predicted_labels)
+precision = precision_score(true_labels, predicted_labels)
+recall = recall_score(true_labels, predicted_labels)
+f1 = f1_score(true_labels, predicted_labels)
+
+print("\nTrue Positives:", TP)
+print("False Positives:", FP)
+print("True Negatives:", TN)
+print("False Negatives:", FN)
+
+print("\nAccuracy:", accuracy)
+print("Precision:", precision)
+print("Recall:", recall)
+print("F1 Score:", f1)
