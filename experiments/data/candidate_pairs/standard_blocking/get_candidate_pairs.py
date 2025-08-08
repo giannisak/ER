e3549b532f5103268c793be60d750008948bd89d
teris45 <lstetsikas3@gmail.com>
2025-08-08 14:02:33 +0300

    Standard Blocking Candidate Pairs changed

--- experiments/data/candidate_pairs/standard_blocking/get_candidate_pairs.py
+++ experiments/data/candidate_pairs/standard_blocking/get_candidate_pairs.py
@@ -25,37 +25,38 @@ def getRatio(iteration_number):
 cc_dict = {
     "BLAST" : BLAST,
     "CardinalityEdgePruning": CardinalityEdgePruning,
-    "CardinalityNodePruning": CardinalityNodePruning(),
-    "ReciprocalCardinalityNodePruning": ReciprocalCardinalityNodePruning(),
-    "ReciprocalWeightedNodePruning" :  ReciprocalWeightedNodePruning(),
-    "WeightedEdgePruning" :  WeightedEdgePruning(),
-    "WeightedNodePruning" :  WeightedNodePruning()
+    "CardinalityNodePruning": CardinalityNodePruning,
+    "ReciprocalCardinalityNodePruning": ReciprocalCardinalityNodePruning,
+    "ReciprocalWeightedNodePruning" :  ReciprocalWeightedNodePruning,
+    "WeightedEdgePruning" :  WeightedEdgePruning,
+    "WeightedNodePruning" :  WeightedNodePruning
 }
 
 
 if __name__ == '__main__':
 
     # Read the JSON file
-    with open('standard_blocking_candidate_pairs/.config.json', 'r') as file:
+    with open("data/candidate_pairs/standard_blocking/.config.json", 'r') as file:
         data_json = json.load(file)
 
 
-
-        for dataset in ['D3']:
+        results  = []
+        for dataset in ['D2','D3', 'D5','D6', 'D7', 'D8']:
+            result = {}
 
             params = data_json.get(dataset, {})
             print(params)
 
-            cp = pd.read_csv(f'candidate_pairs/{dataset}.csv')
+            cp = pd.read_csv(f'data/candidate_pairs/original/{dataset}.csv')
             cp_columns = list(cp.columns)
             clean_files = [cl.replace("clean", "").replace(".csv", "") for cl in cp_columns]
             
 
             sep = '|' if dataset!='D3' else '#'
 
-            d1 = pd.read_csv(f"data_clean/{dataset}/{clean_files[0]}clean.csv", sep=sep)
-            d2 = pd.read_csv(f"data_clean/{dataset}/{clean_files[1]}clean.csv", sep=sep)
-            gt = pd.read_csv(f"data_clean/{dataset}/gtclean.csv", sep=sep)
+            d1 = pd.read_csv(f"data/{dataset}/{clean_files[0]}clean.csv", sep=sep)
+            d2 = pd.read_csv(f"data/{dataset}/{clean_files[1]}clean.csv", sep=sep)
+            gt = pd.read_csv(f"data/{dataset}/gtclean.csv", sep=sep)
             
             data = Data(
                 dataset_1=d1,
@@ -69,27 +70,48 @@ if __name__ == '__main__':
             blocks = standard_blocking.build_blocks(data)
 
             if params['block_purging']:
-                bp = BlockPurging()
+                bp = BlockPurging(smoothing_factor=params['smoothing_factor'])
                 blocks = bp.process(blocks, data)
 
             bf = BlockFiltering(ratio = getRatio(params['block_filtering']))
+            print(bf.ratio)
+            input("Press Enter to continue...")
+
             blocks = bf.process(blocks, data)
 
 
             if "weighting_scheme" in params: 
                 cc = cc_dict[params['comparison_cleaning']](params['weighting_scheme'])
             else:
-                cc = cc_dict[params['comparison_cleaning']]
+                cc = cc_dict[params['comparison_cleaning']]()
             blocks = cc.process(blocks, data)
 
             print(dataset)
 
             ev = cc.evaluate(blocks)
 
-            if ev['Recall %'] > 90:
-                cp_df = cc.export_to_df(blocks)
-                cp_df.columns = cp_columns
-                cp_df.to_csv(f"standard_blocking_candidate_pairs/{dataset}.csv", index=False)
+            print(ev)
+
+            input("Press Enter to continue...")
+
+            # if ev['Recall %'] > 90:
+            cp_df = cc.export_to_df(blocks)
+            cp_df.columns = cp_columns
+            cp_df.to_csv(f"data/candidate_pairs/standard_blocking/{dataset}.csv", index=False)
+            
+            results.append({
+                "dataset" : dataset,
+                "candidate_pair" : 'Standard-Blocking',
+                "total_candidate_pairs" : len(cp_df),
+                'precision': ev['Precision %'],
+                'recall' : ev['Recall %'] ,
+                'f1' : ev["F1 %"]
+            })
+        results_df = pd.DataFrame(results)
+        results_df.to_csv("data/candidate_pairs/candidate_pairs_perforamnce.csv", header=False, index=False, mode='a+')
+        
+
+
 
 
 
