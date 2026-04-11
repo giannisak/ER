6d20ca92c14ed57aa21f92ef526c353dfef00d7a
teris45 <lstetsikas3@gmail.com>
2026-04-11 12:50:32 +0000

    D2 ChatGPT

--- experiments/post-review/scripts/gpt.py
+++ experiments/post-review/scripts/gpt.py
@@ -1,3 +1,163 @@
+"""Main Experiments Code. Check how well LLMs can do the matching"""
+import os
+import time
+import pandas as pd
+import numpy as np
+from litellm.batch_completion.main import batch_completion
+from tqdm import tqdm
+from dotenv import load_dotenv
+from gpt_utils import (
+    _evaluate,
+    _load_dataset,
+    _convert_to_numpy,
+    _create_model,
+    _get_response_time,
+    _get_good_behavior_response_rate
+)
+from examples_blocking import examples_dict_list as blocking_examples
+from examples import examples_dict_list as original_examples
 
 
-from litellm import completion
+if __name__ == "__main__":
+    load_dotenv()
+
+
+    BLOCKING_TYPE = "original"
+    DIR = "D2"
+
+    llms = [
+        "openai/gpt-5.4-mini"
+    ]
+
+
+    RESULTS = f'post-review/results/gpt/{BLOCKING_TYPE}/{DIR}.csv'
+    RESPONSES = f"post-review/responses/{BLOCKING_TYPE}/{DIR}"
+
+    prompts = {
+
+    "p2" : """You are given two record descriptions and your task is to identify
+    if the records refer to the same entity or not.
+
+    You must answer with just one word:
+    True. if the records are referring to the same entity,
+    False. if the records are referring to a different entity."""
+
+    }
+
+    if BLOCKING_TYPE == "original":
+        examples_dict_list = original_examples
+    else:
+        examples_dict_list = blocking_examples
+
+    for examples, examples_dict in tqdm(examples_dict_list.items(),
+                desc="Examples", position=0, leave=True):
+        if examples != "vector_based_examples_dict_1":
+            continue
+
+        for ll in llms:
+            for suffix in ['z', 'ft', 'tf']:
+                if suffix == 'z' and \
+                    examples != "vector_based_examples_dict_1":
+                    continue
+                for prompt_key in ["p2"]:
+                    PROMPT = prompts[prompt_key]
+                    LLM = f'gpt-5.4-{suffix}-{prompt_key}'
+                    # candidate_pairs_dir = "standard_blocking"
+
+                    print(f' ----------- {DIR} {LLM} -------------')
+
+                    dt1_df, dt2_df, cp_df, gt_df, clean_files = _load_dataset(BLOCKING_TYPE, DIR)
+                    dt_1, dt_2, cp, gt_set = _convert_to_numpy(dt1_df, dt2_df, cp_df, gt_df)
+
+                    start = time.time()
+                    responses = []
+                    num_iterations = len(cp)
+
+
+                    dt1_ids = {int(dt1_df.at[i, 'id']): i for i in range(len(dt1_df))}
+                    dt2_ids = {int(dt2_df.at[i, 'id']): i for i in range(len(dt2_df))}
+                    PROMPT = _create_model(LLM, ll, PROMPT,
+                            suffix,
+                            examples_dict, DIR, dt_1,
+                            dt_2, dt1_ids, dt2_ids)
+
+
+
+                    print(f""" --- {LLM} ----\n{PROMPT}\n """)
+                    all_messages = []
+                    for pair in tqdm(cp,
+                            desc="Processing", position=1, leave=False):
+
+                        dt1_id = pair[0]
+                        dt2_id = pair[1]
+
+                        r1 = dt_1[dt1_ids[pair[0]]]
+                        r2 = dt_2[dt2_ids[pair[1]]]
+                        query = f"record 1: {r1}, record 2: {r2}. Answer with True. or False."
+
+                        all_messages.append([
+                            {'role': 'system', 'content': PROMPT},
+                            {'role': 'user', 'content': query},
+                        ])
+
+                        gt_value = (dt1_id, dt2_id) in gt_set
+
+                    batch_responses = batch_completion(
+                        model=ll,
+                        messages=all_messages,
+                        reasoning_effort="none",
+                        stream=False,
+                    )
+
+                    responses = [resp.choices[0].message.content for resp in batch_responses]
+                    end = time.time()
+
+                    cp_df['responses'] =  responses
+
+                    time_seconds =  end - start
+                    _get_response_time(start, end)
+
+
+                    good_behavior_rate = _get_good_behavior_response_rate(responses)
+
+                    cp_df['responses'] = cp_df['responses'].apply(lambda x:
+                            'True' in str(x))
+
+                    cp_df.to_csv(f'{RESPONSES}/{DIR}_{LLM}_{examples}.csv',
+                                index=False)
+
+
+                    precision, recall, f1 = _evaluate(gt_set, cp, responses)
+                    filtered_cp_df = cp_df[cp_df['responses'] == True]
+
+                    u0, counts0 = np.unique(filtered_cp_df['D1'], return_counts=True)
+                    u1, counts1 = np.unique(filtered_cp_df['D2'], return_counts=True)
+
+                    u0 = u0[counts0 == 1]
+                    u1 = u1[counts1 == 1]
+
+                    total_pairs = filtered_cp_df.shape[0]
+
+                    non_conflicts = np.isin(filtered_cp_df['D1'], u0) & np.isin(filtered_cp_df['D2'], u1)
+                    conflict_pct = (1 - non_conflicts.sum() / total_pairs) * 100 if total_pairs > 0 else 0.0
+
+                    results_df = pd.DataFrame(
+                        {
+                            'dataset_1': clean_files[0],
+                            'dataset_2': clean_files[1],
+                            'dataset': DIR,
+                            'model': LLM,
+                            'time (sec)': time_seconds,
+                            'precision': precision,
+                            'recall': recall,
+                            'f1': f1 ,
+                            'good_behavior_response_rate': good_behavior_rate,
+                            "examples" : examples,
+                            'total_matches': filtered_cp_df.shape[0],
+                            "conflicts" : conflict_pct
+                        }, index=[0]
+                    )
+
+                    header = not os.path.exists(RESULTS)
+                    results_df.to_csv(RESULTS, mode='a+',
+                        index=False, header=header, float_format='%.2f')
---
+++ experiments/post-review/scripts/gpt_utils.py
@@ -0,0 +1,200 @@
+import pandas as pd
+import numpy as np
+from sklearn.metrics import f1_score, precision_score, recall_score
+
+
+def _load_dataset(blocking_type, dir):
+    candidate_pairs = f'data/candidate_pairs/{blocking_type}/{dir}.csv'
+    cp_df_with_rows = pd.read_csv(candidate_pairs)
+    cp_columns = list(cp_df_with_rows.columns)
+    clean_files = [cl.replace("clean", "").replace(".csv", "") for cl in cp_columns]
+
+    # data_dir = "giannis"
+    dataset_1 = f'data/{dir}/{clean_files[0]}clean.csv'
+    dataset_2 = f'data/{dir}/{clean_files[1]}clean.csv'
+    groundtruth = f'data/{dir}/gtclean.csv'
+
+
+    sep = '|' if dir!='D3' else '#'
+
+    # read the files (Edit sep if needed)
+    dt1_df = pd.read_csv(dataset_1, sep=sep)
+    dt2_df = pd.read_csv(dataset_2, sep=sep)
+
+    gt_df = pd.read_csv(groundtruth, sep=sep)
+
+
+    if blocking_type == 'original':
+        cp_df = pd.DataFrame({
+            'D1': cp_df_with_rows[cp_columns[0]].map(dt1_df['id']),
+            'D2': cp_df_with_rows[cp_columns[1]].map(dt2_df['id'])
+        })
+    else:
+        cp_df = cp_df_with_rows
+        cp_df.columns = ['D1','D2']
+
+
+    dt1_df = dt1_df.fillna("").astype(str)
+
+
+    dt2_df = dt2_df.fillna("").astype(str)
+
+    return dt1_df, dt2_df, cp_df, gt_df, clean_files
+
+
+def _create_model(llm, ll, prompt, suffix,
+            examples_dict, dir, dt_1, dt_2, dt1_ids, dt2_ids):
+
+    if suffix != 'z':
+        true_flag = True
+        true_example = []
+        false_example = []
+        for pair in examples_dict[dir]:
+            print(pair)
+            r1  =dt_1[dt1_ids[pair[0]]]
+            r2 = dt_2[dt2_ids[pair[1]]]
+
+            if true_flag:
+                true_example.append(f"record 1: {r1}\nrecord 2: {r2}\nAnswer: {true_flag}.")
+            else:
+                false_example.append(f"record 1: {r1}\nrecord 2: {r2}\nAnswer: {true_flag}.")
+            true_flag = False if true_flag else True
+
+        if suffix == 'ft':
+            example_cnt = 1
+
+            for example in false_example:
+                prompt = f"""{prompt}\nExample {example_cnt}:\n{example}\n"""
+                example_cnt += 1
+
+            for example in true_example:
+                prompt = f"""{prompt}\nExample {example_cnt}:\n{example}\n"""
+                example_cnt += 1
+        else:
+            example_cnt = 1
+            for example in true_example:
+                prompt = f"""{prompt}\nExample {example_cnt}:\n{example}\n"""
+                example_cnt += 1
+
+            for example in false_example:
+                prompt = f"""{prompt}\nExample {example_cnt}:\n{example}\n"""
+                example_cnt += 1
+
+    # ollama.create(model=llm, from_=ll, system=prompt)
+    return prompt
+
+
+def _convert_to_numpy(dt1_df, dt2_df, cp_df, gt_df):
+    dt1 = dt1_df.to_numpy()
+    dt2 = dt2_df.to_numpy()
+    cp = cp_df.to_numpy()
+    gt = gt_df.to_numpy()
+
+    # Convert groundtruth to a set of tuples for O(1) lookup
+    gt_set = set(tuple(row) for row in gt)
+
+    # cut the indexes
+    dt1 = dt1[:, 1:]
+    dt2 = dt2[:, 1:]
+
+
+    # concatenate the strings in each column to a single string, omitting empty elements
+    dt1 = np.array([' '.join([x for x in row if isinstance(x, str)]) for row in dt1])
+    dt2 = np.array([' '.join([x for x in row if isinstance(x, str)]) for row in dt2])
+    return dt1, dt2, cp, gt_set
+
+
+def _get_response_time(start, end):
+    # model's response time
+    time_seconds = end - start
+    hours, remainder = divmod(time_seconds, 3600)
+    minutes, seconds = divmod(remainder, 60)
+    print("Response Time: {:02}:{:02}:{:.2f}".
+        format(int(hours), int(minutes), seconds))
+
+def _get_good_behavior_response_rate(responses):
+    good_responses = sum(response == 'True' or response == 'False' for response in responses)
+    good_behavior_rate = good_responses / len(responses) if len(responses) > 0 else 0
+    print("Good Behavior Response Rate:", good_behavior_rate)
+    return good_behavior_rate
+
+def _evaluate(gt_set, cp, responses):
+
+    true_labels = [1 if tuple(pair) in gt_set else 0 for pair in cp]
+    predicted_labels = [1 if resp else 0 for resp in responses]
+
+    precision = precision_score(true_labels, predicted_labels) * 100
+    recall = recall_score(true_labels, predicted_labels) * 100
+    f1 = f1_score(true_labels, predicted_labels) * 100
+    return precision, recall, f1
+
+
+
+#                     # model's responses
+#                     for i in range(len(responses)):
+#                         if responses[i] != 'True' and responses[i] != 'False':
+#                             responses[i] = 'False'
+
+#                     if 'ft' in llm or 'tf' in llm:
+#                         responses_filename = f"{dataset_dir}/{llm}_responses.txt"
+#                         with open(responses_filename, 'w') as file:
+#                             for response in responses:
+#                                 file.write(response + '\n')
+#                         print(f"Responses saved to {responses_filename} for union/intersection.")
+#     if 'total_matches' not in list(results_df.columns):
+#         total_matches = []
+#         d1_conf = []
+#         d2_conf = []
+
+#         for _, row in results_df.iterrows():
+#             llm = row['model']
+#             examples = row['examples']
+#             responses_path = f'responses/{candidate_pairs_dir}/{dataset}/{dataset}_{llm}_{examples}.csv'
+
+#             if '-z' in llm and not os.path.exists(responses_path):
+#                 responses_path = f'responses/{candidate_pairs_dir}/{dataset}/{dataset}_{llm}.csv'
+
+#             responses_df = pd.read_csv(responses_path)
+
+#             if isinstance(responses_df.iloc[0]['responses'], str):
+#                 filtered_cp_df = responses_df[responses_df['responses'] == 'True']
+#             else:
+#                 filtered_cp_df = responses_df[responses_df['responses'] == True]
+
+#             d1_list = filtered_cp_df['D1'].to_list()
+#             d2_list = filtered_cp_df['D2'].to_list()
+#             d1_set = set(d1_list)
+#             d2_set = set(d2_list)
+
+#             total_matches.append(len(d1_list))
+#             d1_conf.append((len(d1_list) - len(d1_set))/len(d1_list))
+#             d2_conf.append((len(d2_list) - len(d2_set))/len(d2_list))
+
+#         results_df['total_matches'] = total_matches
+#         results_df["D1_conflicts"] = d1_conf
+#         results_df['D2_conflicts'] = d2_conf
+#         results_df.to_csv(f'results/{candidate_pairs_dir}/{dataset}.csv', mode='w+', index=False, header=True)
+
+
+
+
+
+
+
+
+#     ui_fun(dataset, candidate_pairs_dir)
+
+
+# for dataset in ['D2', 'D5', 'D6', 'D7', 'D8' ]:
+#     for candidate_pairs_dir in ["original", "standard_blocking"]:
+
+#     # candidate_pairs_dir = "original"
+#         ui_fun(dataset, candidate_pairs_dir)
+
+
+
+# # for candidate_pairs_dir in ["original", "standard_blocking"]:
+#     # dataset = 'D3'
+#     # (dataset, candidate_pairs_dir)
+
+
